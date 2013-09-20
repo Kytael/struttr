8eed35dfee186180a3874b4c91dd1ad3e8d93ce2
Wolf Tivy <wolf@tivy.com>
2013-09-19 22:01:58 -0700

    got basic tech in place

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlalchemy as sa
 
 app = Flask(__name__)
 
+app.debug = True
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def root():
+    return render_template('root.html', hello="hhhh")
+
+@app.route('/boast', methods=['GET', 'POST'])
+def boast():
+    return request.args.get('brag')
+    
