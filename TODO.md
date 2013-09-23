b1cd5eef301f5c4f6e01923c3117c70c2a98bfc5
Wolf Tivy <wolf@tivy.com>
2013-09-22 21:46:16 -0700

    connected to postgres db

--- TODO.md
+++ TODO.md
@@ -1,13 +1,11 @@
 Things to Do
 ============
 
-*   Connect database to site
-
 *   add dependencies for heroku
 
 *   Make it use env variables for heroku
 
-*   Install herkoku postgres addon
+*   Install heroku postgres addon
 
 *   Migrate DB on heroku
 
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask, render_template, request
+from flask import *
 import sqlalchemy as sa
 
 app = Flask(__name__)
 
 app.debug = True
+
+engine = sa.create_engine('postgresql://wolf@localhost/struttr')
+conn = engine.connect()
+
+metadata = sa.MetaData()
+boasts = sa.Table("boasts", metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('description', sa.String))
+
+metadata.create_all(engine)
 
 @app.route('/')
 def root():
@@ -12,5 +22,6 @@ def root():
 
 @app.route('/boast', methods=['GET', 'POST'])
 def boast():
-    return request.args.get('brag')
-    
+    ins = boasts.insert().values(description=request.args.get('brag'))
+    conn.execute(ins)
+    return redirect(url_for('root'))
